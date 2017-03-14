d1faa24e3db53c8657f6434d66277b85efde51c2
Sudarshan <codezarvis@gmail.com>
2017-03-14 06:07:41 +0530

    Changes

--- src/main/java/org/codezarvis/artifactory/hushly/App.java
+++ src/main/java/org/codezarvis/artifactory/hushly/App.java
@@ -4,10 +4,12 @@ package org.codezarvis.artifactory.hushly;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+	public String getAppName() {
+		return "App";
+	}
 }
