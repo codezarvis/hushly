2ae6e29aff341718ad2ea93c7dd42f775db49143
Sudarshan <codezarvis@gmail.com>
2017-03-15 06:18:17 +0530

    Branch Created

---
+++ src/main/java/org/codezarvis/artifactory/hushly/HushCode.java
@@ -0,0 +1,12 @@
+package org.codezarvis.artifactory.hushly;
+
+public class HushCode {
+
+	public String getHush() {
+		return "Hush";
+	}
+	
+	public String getAnotherHush() {
+		return "Another-Hush";
+	}
+}
